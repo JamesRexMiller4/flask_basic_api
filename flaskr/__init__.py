58f5ebb6e679f77baff60d7a25de8383c743b78b
James Miller <james.rex.miller.4@gmail.com>
2020-04-09 09:57:09 -0600

    Initial commit + add CORS

---
+++ flaskr/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sys
+
+
+def create_app(test_config=none):
+  app = Flask(__name__)
+  # VVVVVVVV - replace with setup_db(app)
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/todoapp'
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+  db = SQLAlchemy(app)
+  migrate = Migrate(app, db)
+  # alternatively can import from models directory
+  CORS(app, resources={r='*/api/*': {origins: '*'}})
+
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+    return response
+    
+  @app.route('/')
+  @cross_origin
+  def hello():
+    return jsonify({"message": "Hello World!"})
+
+  @app.route('/smile')
+  def smile():
+    return ':)'
+  return app
